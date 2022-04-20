379a0f8953b8ac919f372e2587a889e9060db885
Collins <mutaic84@gmail.com>
2022-04-20 15:41:33 +0300

    display movie titles on left panel when page loads

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-// Your code here
+document.addEventListener("DOMContentLoaded", () => {
+    const movieTitlesListContainer = document.querySelector("div.list-container");
+    const movieTitlesList = document.getElementById("films")
+
+    fetch("http://localhost:3000/films")
+    .then(response => response.json())
+    .then(movies => {
+        movies.forEach(movie => {
+            console.log(movie)
+            displayMovieTitle(movie)
+            // if (movie.id === 1) {
+            //     displayMovieInfo(movie)
+            // }
+        })
+    })
+
+    function displayMovieTitle(movie) {
+        let item = document.createElement("li")
+        item.setAttribute("class", "film item")
+        item.innerText = movie.title
+        movieTitlesList.append(item)
+
+        
+    }
+})
