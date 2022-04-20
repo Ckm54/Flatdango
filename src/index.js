26cc715a272f4487bd03c362d399e318f8ac6cb6
Collins <mutaic84@gmail.com>
2022-04-20 15:58:58 +0300

    display movie info and first movie when page loads

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const movieTitlesListContainer = document.querySelector("div.list-container");
-    const movieTitlesList = document.getElementById("films")
+    const movieTitlesList = document.getElementById("films");
+    const movieCard = document.querySelector("div.card")
 
     fetch("http://localhost:3000/films")
     .then(response => response.json())
@@ -29,5 +30,23 @@ document.addEventListener("DOMContentLoaded", () => {
         const imageTag = document.getElementById("poster")
         imageTag.setAttribute("src", movie.poster)
         imageTag.setAttribute("alt", movie.title)
+
+        const movieInfo = `
+            <div id="title" class="title">${movie.title}</div>
+            <div id="runtime" class="meta">${movie.runtime} minutes</div>
+            <div class="content">
+            <div class="description">
+                <div id="film-info">${movie.description}</div>
+                <span id="showtime" class="ui label">${movie.showtime}</span>
+                <span id="ticket-num">${movie.capacity - movie.tickets_sold}</span> remaining tickets
+            </div>
+            </div>
+            <div class="extra content">
+            <button id="buy-ticket" class="ui orange button">
+                Buy Ticket
+            </button>
+            </div>
+        `
+        movieCard.innerHTML = movieInfo
     }
 })
